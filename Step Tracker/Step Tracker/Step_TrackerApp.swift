0c6361d167bcb840234dd1eee483f6e30668ff7e
Tyler Rhodes <trhod007@gmail.com>
2024-04-24 18:48:06 -0400

    created xcode project

---
+++ Step Tracker/Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Tyler Rhodes on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
