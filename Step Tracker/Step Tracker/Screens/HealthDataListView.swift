f4b925c19eaec1205f35182bf7ffce558acf6c4a
ty-rhodes <104220819+ty-rhodes@users.noreply.github.com>
2024-06-28 10:53:06 -0400

    text input validation

--- Step Tracker/Step Tracker/Screens/HealthDataListView.swift
+++ Step Tracker/Step Tracker/Screens/HealthDataListView.swift
@@ -57,9 +57,9 @@ struct HealthDataListView: View {
             .navigationTitle(metric.title)
             .alert(isPresented: $isShowingAlert, error: writeError) { writeError in
                 switch writeError {
-                case .authNotDetermined, . noData, .unableToCompleteRequest:
+                case .authNotDetermined, . noData, .unableToCompleteRequest, .invalidValue:
                     EmptyView()
-                case .sharingDenied(let quantityType):
+                case .sharingDenied(_):
                     Button("Settings") {
                         UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
                     }
@@ -72,10 +72,16 @@ struct HealthDataListView: View {
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Add Data") {
+                        guard let value = Double(valueToAdd) else {
+                            writeError = .invalidValue
+                            isShowingAlert = true
+                            valueToAdd = ""
+                            return
+                        }
                         Task {
                             if metric == .steps {
                                 do {
-                                    try await hkManager.addStepData(for: addDataDate, value: Double(valueToAdd)!)
+                                    try await hkManager.addStepData(for: addDataDate, value: value)
                                     try await hkManager.fetchStepCount()
                                     isShowingAddData = false
                                 } catch STError.sharingDenied(let quantityType) {
@@ -88,7 +94,7 @@ struct HealthDataListView: View {
                                 
                             } else {
                                 do {
-                                    try await hkManager.addWeightData(for: addDataDate, value: Double(valueToAdd)!)
+                                    try await hkManager.addWeightData(for: addDataDate, value: value)
                                     try await hkManager.fetchWeights()
                                     try await hkManager.fetchWeighForDifferentials()
                                     isShowingAddData = false
